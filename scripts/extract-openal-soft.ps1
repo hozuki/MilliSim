914e0a884d4fa1facb5a1cfc26fee4983e298101
George Wu <uiharu@buaa.edu.cn>
2018-03-14 21:54:18 +0100

    OpenAL script fix

--- scripts/extract-openal-soft.ps1
+++ scripts/extract-openal-soft.ps1
@@ -2,19 +2,18 @@ param (
     [Parameter(Mandatory=$true)][string]$destination
 )
 
-echo "Downloading OpenAL-Soft (v1.18.2/Windows)..."
-(New-Object Net.WebClient).DownloadFile('http://kcat.strangesoft.net/openal-binaries/openal-soft-1.18.2-bin.zip', 'C:\openal-soft.zip')
+$openAlSoftVersion = '1.18.2'
+
+echo "Downloading OpenAL-Soft ($openAlSoftVersion/Windows)..."
+(New-Object Net.WebClient).DownloadFile('http://kcat.strangesoft.net/openal-binaries/openal-soft-' + $openAlSoftVersion + '-bin.zip', 'C:\openal-soft.zip')
 
 echo "Extracting OpenAL-Soft..."
 
-$7zCommand = '7z e C:\openal-soft.zip -y -o"$destination" openal-soft-1.18.2-bin/bin/Win32/soft_oal.dll'
+$7zCommand = '7z x C:\openal-soft.zip -y -oC:\openal-soft-install\ * -r'
 $block = [scriptblock]::Create($7zCommand)
 
 Invoke-Command -ScriptBlock $block
 
-echo "Renaming to openal32.dll..."
-
-$openAlDllPath = [System.IO.Path]::Combine($destination, "soft_oal.dll")
-$openAlDllNewName = "openal32.dll"
-
-Rename-Item -Path $openAlDllPath -NewName $openAlDllNewName
+$openAlDllNewName = [System.IO.Path]::Combine($destination, "openal32.dll")
+echo 'Moving to "$openAlDllNewName"...'
+Move-Item -Path 'C:\openal-soft-install\openal-soft-' + $openAlSoftVersion + '-bin\bin\Win32\soft_oal.dll' -Destination $openAlDllNewName
